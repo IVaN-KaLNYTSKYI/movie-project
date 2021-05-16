6e92ed7f0556e90f704f0263a851a62011c9a5a0
IVaN-KaLNYTSKYI <ivan.kalnytskyi@gmail.com>
2021-05-16 16:20:04 +0300

    no done like

--- src/components/like/Like.js
+++ src/components/like/Like.js
@@ -1,7 +1,27 @@
+
 export default function Like () {
+    const localData=[];
+    const movieData=[];
+    for(let i in localStorage ){
+        if(i.startsWith("like")){
+            localData.push(i)
+        }
+    }
+    // eslint-disable-next-line array-callback-return
+    localData.map((value)=>{
+            return movieData.push(JSON.parse(localStorage.getItem(`${value}`)))
+        }
+    )
+    console.log(localData)
+    console.log(movieData)
   return(
       <div>
-
+          {
+movieData.map(value => <div key={value.id}>{value.id}
+<img
+     src={'https://image.tmdb.org/t/p/w200' + value.photo}
+     alt={value.id}/></div>)
+          }
       </div>
   )
 }
--- src/components/movie-list/MovieList.js
+++ src/components/movie-list/MovieList.js
@@ -1,25 +1,31 @@
 import './MovieList.css'
 import {Link} from "react-router-dom";
-import {useEffect, useState} from 'react';
+import {useState} from "react";
 export default function MovieList({item}) {
-
+    let [flagColor, setFlagColor] = useState("green");
+    let [text, setText] = useState("");
+    setText("yes")
 const like=(e)=>{
     e.preventDefault();
     const like={
         id:item.id,
         photo:item.poster_path
     }
-    let arr=[]
-    arr.push(like)
-    localStorage.setItem("like"+item.id,JSON.stringify(arr))
+
+    setFlagColor("red")
+    localStorage.setItem("text"+item.id,JSON.stringify(text))
+        localStorage.setItem("like" + item.id, JSON.stringify(like))
+
 }
+
 
     return (
             <div className={"cart-box"}>
                 <img className={"img-cart"} src={'https://image.tmdb.org/t/p/w200' + item.poster_path} alt={item.id}/>
                <Link to={"/movie/"+item.id}><span className={"movie-title"}>{item.title}</span></Link>
                 <div className={"movie-rating"}><span>{item.vote_average}</span></div>
-                <button onClick={like}>like</button>
+                <button onClick={like} style={{background:`${flagColor}`}}>like</button>
+                <span>{text}</span>
             </div>
     )
 }
